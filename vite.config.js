9a9d5590c984433f0da99611a1082056959a2df4
Bhumika <bhumikasudarshani@gmail.com>
2026-01-28 13:58:30 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CampusePulse./',  // 👈 IMPORTANT: your repo name EXACTLY
+  base: '/CampusePulse/',  // 👈 IMPORTANT: your repo name EXACTLY
 })
